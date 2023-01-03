ba2665270238012c6562c4dbb45d11960f274e3b
DELL <rasheed.fisal2015@gmail.com>
2023-01-03 06:57:29 +0200

    add new origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:3001", "http://localhost:3002"];
+const allowedOrigins = [
+  "http://localhost:3001",
+  "http://localhost:3002",
+  "https://mostaql.vercel.app",
+];
 
 module.exports = allowedOrigins;
