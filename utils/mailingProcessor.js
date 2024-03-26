246cd77d76ae5811882bb4e229c80f019af1e216
DELL <rasheed.fisal2015@gmail.com>
2024-03-26 02:45:55 +0200

    emailing with sendgrid

--- utils/mailingProcessor.js
+++ utils/mailingProcessor.js
@@ -1,5 +1,6 @@
-var nodemailer = require("nodemailer");
-
+// var nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 // var transporter = nodemailer.createTransport({
 //   service: "outlook",
 //   auth: {
@@ -7,34 +8,41 @@ var nodemailer = require("nodemailer");
 //     pass: process.env.MAILING_PWD,
 //   },
 // });
-var transporter = nodemailer.createTransport({
-  host: "mail.sudasofttech.com",
-  port: 587,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: process.env.MAILING_EMAIL, // your domain email address
-    pass: process.env.MAILING_PWD, // your password
-  },
-});
+// var transporter = nodemailer.createTransport({
+//   host: "mail.sudasofttech.com",
+//   port: 587,
+//   secure: false, // true for 465, false for other ports
+//   auth: {
+//     user: process.env.MAILING_EMAIL, // your domain email address
+//     pass: process.env.MAILING_PWD, // your password
+//   },
+// });
 
 exports.sendMail = async function (emailInfo) {
-  const formData = new FormData();
-  formData.append("ToEmail", emailInfo.to);
-  formData.append("Subjuect", emailInfo.subject);
-  formData.append("Body", emailInfo.html);
+  // const formData = new FormData();
+  // formData.append("ToEmail", emailInfo.to);
+  // formData.append("Subjuect", emailInfo.subject);
+  // formData.append("Body", emailInfo.html);
   try {
-    const response = await fetch(process.env.EMAIL_SERVICE_ENDPOINT_URL, {
-      method: "POST",
-      headers: {
-        XApiKey: process.env.EMAIL_KEY_AUTH,
-      },
-      // body: JSON.stringify({
-      //   email: email.value,
-      //   password: password.value,
-      // }),
-      body: formData,
-    });
-    console.log(response.statusText);
+    // const response = await fetch(process.env.EMAIL_SERVICE_ENDPOINT_URL, {
+    //   method: "POST",
+    //   headers: {
+    //     XApiKey: process.env.EMAIL_KEY_AUTH,
+    //   },
+    //   // body: JSON.stringify({
+    //   //   email: email.value,
+    //   //   password: password.value,
+    //   // }),
+    //   body: formData,
+    // });
+    const msg = {
+      to: emailInfo.to, // Change to your recipient
+      from: process.env.SENDGRID_USER, // Change to your verified sender
+      subject: emailInfo.subject,
+      //text: "and easy to do anywhere, even with Node.js",
+      html: emailInfo.html,
+    };
+    await sgMail.send(msg);
   } catch (error) {
     console.log(error);
   }
